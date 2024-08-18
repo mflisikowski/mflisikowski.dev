38bd0c61b1e1a180a998ff31b8d501b1ec984ab6
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-08-18 21:26:09 +0200

    feat(components): update Container component

---
+++ components/container.tsx
@@ -0,0 +1,37 @@
+import { cn } from "@/utils/cn";
+
+type ContainerProps<T extends React.ElementType> = {
+	classNameInner?: string;
+	className?: string;
+	children: React.ReactNode;
+	as?: T;
+};
+
+const ContainerInner = <T extends React.ElementType = "div">({
+	as,
+	className,
+	children,
+}: Omit<React.ComponentPropsWithoutRef<T>, keyof ContainerProps<T>> & ContainerProps<T>) => {
+	const Component = as ?? "div";
+
+	return (
+		<Component className={cn("mx-auto max-w-7xl px-6 lg:px-8", className)}>{children}</Component>
+	);
+};
+
+export function Container<T extends React.ElementType = "div">({
+	as,
+	classNameInner,
+	className,
+	children,
+}: Omit<React.ComponentPropsWithoutRef<T>, keyof ContainerProps<T>> & ContainerProps<T>) {
+	const Component = as ?? "div";
+
+	return (
+		<Component className={cn("mx-auto max-w-7xl px-6 lg:px-8", className)}>
+			<ContainerInner className={cn("mx-auto max-w-2xl lg:max-w-none", classNameInner)}>
+				<>{children}</>
+			</ContainerInner>
+		</Component>
+	);
+}
