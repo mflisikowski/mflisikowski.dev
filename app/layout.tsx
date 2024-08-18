4ad0e33d483170e06af2a2d814b0ccf3f50e64a4
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-08-18 22:17:19 +0200

    feat(SEO): enhance metadata in RootLayout with SEO optimizations
    - Updated `metadata` in `RootLayout` to include a dynamic title with a default and template format.
    - Added a detailed description highlighting the mission-focused approach and innovative solutions.
    - Included a comprehensive list of keywords related to the developer's expertise and services.
    - Added author information with name and URL for better SEO and attribution.

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,34 @@ import { cn } from "@/utils/cn";
 import "./globals.css";
 
 export const metadata: Metadata = {
+	title: {
+		default: `Mateusz Flisikowski Mission Focused Developer`,
+		template: `%s | Mateusz Flisikowski`,
+	},
+	description: `Mateusz Flisikowski, deweloper z pasją do tworzenia innowacyjnych rozwiązań, posiada podejście skoncentrowane na misji, nowoczesnych technologiach i rozwoju oprogramowania.`,
+	keywords: [
+		"Mateusz Flisikowski",
+		"TypeScript Developer",
+		"Node.js Developer",
+		"JavaScript Developer",
+		"Next.js Developer",
+		"React Developer",
+		"Frontend Developer",
+		"Tworzenie aplikacji webowych",
+		"Tworzenie stron internetowych",
+		"Programista JavaScript",
+		"Programista TypeScript",
+		"Innowacyjne rozwiązania",
+		"Technologia zorientowana na misję",
+		"Tworzenie stron internetowych dostosowanych do potrzeb klienta",
+		"Tworzenie aplikacji webowych dostosowanych do potrzeb klienta",
+		"Innowacyjne rozwiązania technologiczne dla małych i średnich przedsiębiorstw",
+		"Rozwój oprogramowania zorientowany na potrzeby biznesowe klienta",
+	],
+	authors: {
+		name: "Mateusz Flisikowski",
+		url: "www.mflisikowski.dev",
+	},
 };
 
 const CalSans = localFont({
