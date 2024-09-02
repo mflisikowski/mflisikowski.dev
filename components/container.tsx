7ec8eafd833ec2410eee12931e1a975930656958
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-09-02 20:00:59 +0200

    refactor(Container): improve flexibility and prop handling

--- components/container.tsx
+++ components/container.tsx
@@ -24,11 +24,12 @@ export function Container<T extends React.ElementType = "div">({
   classNameInner,
   className,
   children,
+  ...props
 }: Omit<React.ComponentPropsWithoutRef<T>, keyof ContainerProps<T>> & ContainerProps<T>) {
   const Component = as ?? "div";
 
   return (
-    <Component className={cn("mx-auto max-w-7xl px-6 lg:px-8", className)}>
+    <Component className={cn("mx-auto max-w-7xl", className)} {...props}>
       <ContainerInner className={cn("mx-auto max-w-2xl lg:max-w-none", classNameInner)}>
         <>{children}</>
       </ContainerInner>
