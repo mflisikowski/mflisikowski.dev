82dc2b610960b6244d3e1ec4071550e4627313ac
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-09-02 21:32:38 +0200

    feat(components): add AnimatedTextLetters component

---
+++ components/framer-motion/animated-text-letters.tsx
@@ -0,0 +1,82 @@
+import { type Variants, motion } from "framer-motion";
+import React from "react";
+
+import { cn } from "@/utils/cn";
+
+interface AnimationConfig {
+  initialY?: number;
+  finalY?: number;
+  duration?: number;
+  delay?: number;
+  repeatDelay?: number;
+  startDelay?: number;
+}
+
+interface AnimatedTextLettersProps {
+  text: string;
+  className?: string;
+  infinite?: boolean;
+  animated?: boolean;
+  animationConfig?: AnimationConfig;
+}
+
+export const AnimatedTextLetters: React.FC<AnimatedTextLettersProps> = ({
+  text,
+  className,
+  infinite = false,
+  animated = true,
+  animationConfig = {},
+}) => {
+  const {
+    initialY = 0,
+    finalY = 0,
+    duration = 0.01,
+    delay = 0.01,
+    repeatDelay = 5,
+    startDelay = 0,
+  } = animationConfig;
+
+  const letterVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      y: initialY,
+    },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: finalY,
+      transition: {
+        delay: startDelay + i * delay, // Add startDelay to the delay calculation
+        duration: duration,
+        repeat: infinite ? Infinity : 0,
+        repeatType: infinite ? "reverse" : undefined,
+        repeatDelay: infinite ? repeatDelay : undefined,
+      },
+    }),
+  };
+
+  const lines = text.split("\n");
+
+  return (
+    <div className={cn("text-balance", className)}>
+      {lines.map((line, lineIndex) => (
+        <p key={lineIndex} className={cn("text-base", className)}>
+          {line.split("").map((char, charIndex) =>
+            animated ? (
+              <motion.span
+                key={`${char}-${lineIndex}-${charIndex}`}
+                custom={charIndex + lineIndex * line.length}
+                variants={letterVariants}
+                initial="hidden"
+                animate="visible"
+              >
+                {char}
+              </motion.span>
+            ) : (
+              <span key={`${char}-${lineIndex}-${charIndex}`}>{char}</span>
+            ),
+          )}
+        </p>
+      ))}
+    </div>
+  );
+};
