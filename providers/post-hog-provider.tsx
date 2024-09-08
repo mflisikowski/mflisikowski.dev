5c32ec44155407094427a4cd2ae3440997678ed0
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-09-08 18:24:12 +0200

    refactor(analytics): enhance PostHog initialization and consent handling
    - Implement separate initialization and consent-based configuration for PostHog
    - Add state to track PostHog initialization status
    - Initialize PostHog with opt-out and disabled persistence by default
    - Dynamically update PostHog configuration based on user consent
    - Always render PostHogProvider regardless of consent status

--- providers/post-hog-provider.tsx
+++ providers/post-hog-provider.tsx
@@ -2,33 +2,48 @@
 
 import posthog from "posthog-js";
 import { PostHogProvider as Provider } from "posthog-js/react";
-import { type FC, type PropsWithChildren, useEffect } from "react";
+import { type FC, type PropsWithChildren, useEffect, useState } from "react";
 
 import { useConsentStore } from "@/stores/consent-store";
 
 export const PostHogProvider: FC<PropsWithChildren> = ({ children }) => {
   const { isConsentGiven, checkConsent } = useConsentStore();
+  const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
     checkConsent();
-  }, [checkConsent]);
 
-  useEffect(() => {
-    if (isConsentGiven) {
+    if (!isInitialized) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
         api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
         person_profiles: "identified_only",
         capture_pageview: false,
         debug: process.env.NODE_ENV === "development",
         persistence_name: "posthog",
+        opt_out_capturing_by_default: true,
+        disable_persistence: true,
+        loaded: () => {
+          setIsInitialized(true);
+        },
       });
     }
-  }, [isConsentGiven]);
+  }, [checkConsent, isInitialized]);
+
+  useEffect(() => {
+    if (isInitialized) {
+      if (isConsentGiven) {
+        posthog.opt_in_capturing();
+        posthog.set_config({ disable_persistence: false });
+      } else {
+        posthog.opt_out_capturing();
+        posthog.set_config({ disable_persistence: true });
+      }
+    }
+  }, [isConsentGiven, isInitialized]);
 
   return (
-    <>
-      {isConsentGiven && <Provider client={posthog}>{children}</Provider>}
-      {!isConsentGiven && children}
-    </>
+    <Provider client={posthog}>
+      <>{children}</>
+    </Provider>
   );
 };
