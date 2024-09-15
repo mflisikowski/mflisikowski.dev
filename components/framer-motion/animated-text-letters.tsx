0097c30b5547ff46689514019e527d482c249b1b
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-09-15 15:14:10 +0200

    refactor(components): Convert AnimatedTextLetters to a forwardRef component

--- components/framer-motion/animated-text-letters.tsx
+++ components/framer-motion/animated-text-letters.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { type Variants, motion } from "framer-motion";
-import React from "react";
+import React, { forwardRef } from "react";
 
 import { cn } from "@/utils/cn";
 
@@ -22,63 +22,61 @@ interface AnimatedTextLettersProps {
   animationConfig?: AnimationConfig;
 }
 
-export const AnimatedTextLetters: React.FC<AnimatedTextLettersProps> = ({
-  text,
-  className,
-  infinite = false,
-  animated = true,
-  animationConfig = {},
-}) => {
-  const {
-    initialY = 0,
-    finalY = 0,
-    duration = 0.01,
-    delay = 0.01,
-    repeatDelay = 5,
-    startDelay = 0,
-  } = animationConfig;
+export const AnimatedTextLetters = forwardRef<HTMLDivElement, AnimatedTextLettersProps>(
+  ({ animationConfig = {}, className, infinite = false, animated = true, text }, ref) => {
+    const {
+      initialY = 0,
+      finalY = 0,
+      duration = 0.01,
+      delay = 0.01,
+      repeatDelay = 5,
+      startDelay = 0,
+    } = animationConfig;
 
-  const letterVariants: Variants = {
-    hidden: {
-      opacity: 0,
-      y: initialY,
-    },
-    visible: (i: number) => ({
-      opacity: 1,
-      y: finalY,
-      transition: {
-        delay: startDelay + i * delay,
-        duration: duration,
-        repeat: infinite ? Infinity : 0,
-        repeatType: infinite ? "reverse" : undefined,
-        repeatDelay: infinite ? repeatDelay : undefined,
+    const letterVariants: Variants = {
+      hidden: {
+        opacity: 0,
+        y: initialY,
       },
-    }),
-  };
+      visible: (i: number) => ({
+        opacity: 1,
+        y: finalY,
+        transition: {
+          delay: startDelay + i * delay,
+          duration: duration,
+          repeat: infinite ? Infinity : 0,
+          repeatType: infinite ? "reverse" : undefined,
+          repeatDelay: infinite ? repeatDelay : undefined,
+        },
+      }),
+    };
 
-  const lines = text.split("\n");
+    const lines = text.split("\n");
 
-  return (
-    <div className={cn("text-balance", className)}>
-      {lines.map((line, lineIndex) => (
-        <p key={lineIndex} className={cn("text-base", className)}>
-          {line.split("").map((char, charIndex) =>
-            animated ? (
-              <motion.span
-                key={`${char}-${lineIndex}-${charIndex}`}
-                custom={charIndex + lineIndex * line.length}
-                variants={letterVariants}
-                initial="hidden"
-                animate="visible"
-              >
-                {char}
-              </motion.span>
-            ) : (
-              <span key={`${char}-${lineIndex}-${charIndex}`}>{char}</span>
-            ),
-          )}
-        </p>
-      ))}
-    </div>
-  );
-};
+    return (
+      <div ref={ref} className={cn("text-balance", className)}>
+        {lines.map((line, lineIndex) => (
+          <p key={lineIndex} className={cn("text-base", className)}>
+            {line.split("").map((char, charIndex) =>
+              animated ? (
+                <motion.span
+                  key={`${char}-${lineIndex}-${charIndex}`}
+                  custom={charIndex + lineIndex * line.length}
+                  variants={letterVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  {char}
+                </motion.span>
+              ) : (
+                <span key={`${char}-${lineIndex}-${charIndex}`}>{char}</span>
+              ),
+            )}
+          </p>
+        ))}
+      </div>
+    );
+  },
+);
+
+AnimatedTextLetters.displayName = "AnimatedTextLetters";
