deb6a861264c0a7d2110f3b72958c5be9be01ed6
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-11-19 15:02:11 +0100

    style(footer): refactor footer with custom height utility
    - Remove Container component
    - Add h-footer class
    - Adjust padding and flex layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,16 @@
 import { CopyrightIcon } from "lucide-react";
 
-import { Container } from "@/components/container";
-
 export function Footer() {
   return (
-    <Container as="footer" className="relative z-10 w-full">
-      <div className="flex flex-col items-center justify-between space-y-4 lg:flex-row">
-        <p className="text-center font-mono text-sm tracking-tight lg:text-left">
-          With a passion for creating innovative solutions.
-        </p>
-        <p className="flex items-center gap-2 font-mono text-sm tracking-wide">
-          <CopyrightIcon className="inline h-4 w-4" />
-          {new Date().getFullYear()}
-        </p>
-      </div>
-    </Container>
+    <footer className="relative z-10 flex w-full flex-col items-center justify-between px-4 h-footer lg:flex-row">
+      <p className="text-center font-mono text-sm tracking-tight lg:text-left">
+        With a passion for creating innovative solutions.
+      </p>
+
+      <p className="flex items-center gap-2 font-mono text-sm tracking-wide">
+        <CopyrightIcon className="inline h-4 w-4" />
+        {new Date().getFullYear()}
+      </p>
+    </footer>
   );
 }
