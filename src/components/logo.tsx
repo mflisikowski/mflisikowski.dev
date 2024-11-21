42aab93def4fae1286c114bd79b2bcce321e60a5
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-11-21 15:28:01 +0100

    style(logo): remove NotionAvatar component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
-import { NotionAvatar } from "@/components/notion-avatar";
-
 import { cn } from "@/utils/cn";
 
 type LogoProps = {
