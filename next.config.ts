8445b7cc4014f318542fa6a09300f7586a0903b0
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-11-21 07:46:30 +0100

    refactor!: remove Vercel toolbar

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-import withVercelToolbarPlugin from '@vercel/toolbar/plugins/next';
 
-const withVercelToolbar = withVercelToolbarPlugin();
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {};
 
-export default withVercelToolbar(withNextIntl(nextConfig));
+export default withNextIntl(nextConfig);
