91121b992936e19147bea8eb81a4a28bf5551d71
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-11-27 16:21:26 +0100

    feat: extend button variants with link and clear styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -46,12 +46,14 @@ const buttonVariants = cva(
           active:shadow-none
           active:translate-y-0
         `,
+        link: "h-auto p-0",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        clear: "h-auto p-0",
       },
     },
     defaultVariants: {
