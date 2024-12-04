07088aa52941978617928cc9dec4a0b9682f8233
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-04 19:02:57 +0100

    refactor(checkbox-field): implement deepMerge for config handling

--- src/payload/fields/checkbox.ts
+++ src/payload/fields/checkbox.ts
@@ -1,16 +1,17 @@
 import type { CheckboxField } from "payload";
+import { deepMerge } from "payload";
 
 export type CheckboxFieldProps = (overrides?: Partial<CheckboxField>) => CheckboxField;
 
-export const checkboxField: CheckboxFieldProps = (overrides = {}): CheckboxField => {
-  return {
-    localized: true,
-    name: "checkbox",
-    type: "checkbox",
+export const checkboxField: CheckboxFieldProps = (overrides = {}) =>
+  deepMerge<CheckboxField>(
+    {
+      // @ts-expect-error - TFunction type is not automatically merged with the default translations
+      label: ({ t }) => t("custom-checkbox"),
 
-    // @ts-expect-error - TFunction type is not automatically merged with the default translations
-    label: ({ t }) => t("custom-checkbox"),
-
-    ...overrides,
-  };
-};
+      localized: true,
+      name: "checkbox",
+      type: "checkbox",
+    },
+    overrides,
+  );
