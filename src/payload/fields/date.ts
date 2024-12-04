7e8d4ac9a5eb5c57dc2a1c644284e5a913dec589
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-04 19:05:42 +0100

    refactor(date-field): implement deepMerge for config handling

--- src/payload/fields/date.ts
+++ src/payload/fields/date.ts
@@ -1,17 +1,18 @@
 import type { DateField } from "payload";
+import { deepMerge } from "payload";
 
 export type DateFieldProps = (overrides?: Partial<DateField>) => DateField;
 
-export const dateField: DateFieldProps = (overrides = {}): DateField => {
-  return {
-    localized: true,
-    required: true,
-    name: "date",
-    type: "date",
+export const dateField: DateFieldProps = (overrides = {}) =>
+  deepMerge<DateField>(
+    {
+      // @ts-expect-error - TFunction type is not automatically merged with the default translations
+      label: ({ t }) => t("custom-date"),
 
-    // @ts-expect-error - TFunction type is not automatically merged with the default translations
-    label: ({ t }) => t("custom-date"),
-
-    ...overrides,
-  };
-};
+      localized: true,
+      required: true,
+      name: "date",
+      type: "date",
+    },
+    overrides,
+  );
