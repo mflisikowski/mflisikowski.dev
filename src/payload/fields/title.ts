1d96a17a87926daab8838dfc311c26cae2303aa2
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-04 19:07:03 +0100

    refactor(title-field): implement deepMerge for config handling

--- src/payload/fields/title.ts
+++ src/payload/fields/title.ts
@@ -1,15 +1,16 @@
 import type { TextField } from "payload";
+import { deepMerge } from "payload";
 
-export const titleField = (overrides = {}): TextField => {
-  return {
-    localized: true,
-    required: true,
-    name: "title",
-    type: "text",
+export const titleField = (overrides = {}) =>
+  deepMerge<TextField>(
+    {
+      // @ts-expect-error - TFunction type is not automatically merged with the default translations
+      label: ({ t }) => t("custom-title"),
 
-    // @ts-expect-error - TFunction type is not automatically merged with the default translations
-    label: ({ t }) => t("custom-title"),
-
-    ...overrides,
-  };
-};
+      localized: true,
+      required: true,
+      name: "title",
+      type: "text",
+    },
+    overrides,
+  );
