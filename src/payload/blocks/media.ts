2146df08c7007bd24a1a7c8c55b089f02484093c
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-05 01:51:26 +0100

    refactor(media-block): extract fields and use shared field components

--- src/payload/blocks/media.ts
+++ src/payload/blocks/media.ts
@@ -1,32 +1,39 @@
 import type { Block } from "payload";
 
+import { richTextField } from "@/payload/fields/rich-text";
+import { selectField } from "@/payload/fields/select";
+import { uploadField } from "@/payload/fields/upload";
+
+const mediaBlockPosition = selectField({
+  defaultValue: "default",
+  name: "position",
+  options: [
+    {
+      label: "Default",
+      value: "default",
+    },
+    {
+      label: "Wide",
+      value: "wide",
+    },
+  ],
+});
+
+const mediaBlockCaption = richTextField({
+  name: "caption",
+});
+
+const mediaBlockUpload = uploadField({
+  required: true,
+  name: "media",
+});
+
 export const MediaBlock: Block = {
   fields: [
-    {
-      defaultValue: "default",
-      name: "position",
-      type: "select",
-      options: [
-        {
-          label: "Default",
-          value: "default",
-        },
-        {
-          label: "Wide",
-          value: "wide",
-        },
-      ],
-    },
-    {
-      relationTo: "media",
-      required: true,
-      type: "upload",
-      name: "media",
-    },
-    {
-      name: "caption",
-      type: "richText",
-    },
+    //
+    mediaBlockPosition,
+    mediaBlockCaption,
+    mediaBlockUpload,
   ],
 
   slug: "mediaBlock",
