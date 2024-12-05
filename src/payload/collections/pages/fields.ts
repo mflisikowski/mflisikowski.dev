bd66e33159673640f52464f1ced97e644574f83a
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-05 14:19:54 +0100

    refactor(imports): remove unused block imports

--- src/payload/collections/pages/fields.ts
+++ src/payload/collections/pages/fields.ts
@@ -2,9 +2,6 @@
 import { MetaDescriptionField, MetaImageField, MetaTitleField, OverviewField, PreviewField } from "@payloadcms/plugin-seo/fields";
 import type { CollectionConfig } from "payload";
 
-import { ContentBlock } from "@/payload/blocks/content";
-import { CtaBlock } from "@/payload/blocks/cta";
-import { MediaBlock } from "@/payload/blocks/media";
 import { pagePublishedAt } from "@/payload/custom-fields/published-at";
 import { slugField } from "@/payload/custom-fields/slug";
 import { titleField } from "@/payload/custom-fields/title";
