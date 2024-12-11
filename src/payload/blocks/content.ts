30a794696bf502dc13d85f588e87e91ef94c9d7b
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-11 09:51:32 +0100

    refactor(blocks): simplify content block field imports

--- src/payload/blocks/content.ts
+++ src/payload/blocks/content.ts
@@ -1,28 +1,9 @@
 import type { Block } from "payload";
 
-// prettier-ignore
-import { gridLayoutColumnOneField, gridLayoutColumnSelectField, gridLayoutColumnThreeField, gridLayoutColumnTwoField } from "@/payload/custom-fields/grid-layout";
-// prettier-ignore
-import { leadingHeaderCheckboxField, leadingHeaderRichTextField } from "@/payload/custom-fields/leading-header";
-
-const leadingHeaderCheckbox = leadingHeaderCheckboxField();
-const leadingHeaderRichText = leadingHeaderRichTextField();
-
-const gridLayoutColumnSelect = gridLayoutColumnSelectField();
-const gridLayoutColumnThree = gridLayoutColumnThreeField();
-const gridLayoutColumnOne = gridLayoutColumnOneField();
-const gridLayoutColumnTwo = gridLayoutColumnTwoField();
+import { gridField } from "@/payload/custom-fields/grid-layout";
 
 export const ContentBlock: Block = {
-  fields: [
-    leadingHeaderCheckbox,
-    leadingHeaderRichText,
-
-    gridLayoutColumnSelect,
-    gridLayoutColumnOne,
-    gridLayoutColumnTwo,
-    gridLayoutColumnThree,
-  ],
+  fields: [gridField],
 
   slug: "contentBlock",
 };
