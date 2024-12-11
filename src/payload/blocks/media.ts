6f7b8e1118441d3970fa9efbc1d690fbf577a586
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-11 11:27:14 +0100

    refactor(fields): create localized multimedia field group

--- src/payload/blocks/media.ts
+++ src/payload/blocks/media.ts
@@ -1,40 +1,8 @@
 import type { Block } from "payload";
 
-import { richTextField } from "@/payload/fields/rich-text";
-import { selectField } from "@/payload/fields/select";
-import { uploadField } from "@/payload/fields/upload";
-
-const mediaBlockPosition = selectField({
-  defaultValue: "default",
-  name: "position",
-  options: [
-    {
-      label: "Default",
-      value: "default",
-    },
-    {
-      label: "Wide",
-      value: "wide",
-    },
-  ],
-});
-
-const mediaBlockCaption = richTextField({
-  name: "caption",
-});
-
-const mediaBlockUpload = uploadField({
-  required: true,
-  name: "media",
-});
+import { multimediaField } from "@/payload/custom-fields/multimedia";
 
 export const MediaBlock: Block = {
-  fields: [
-    //
-    mediaBlockPosition,
-    mediaBlockCaption,
-    mediaBlockUpload,
-  ],
-
+  fields: [multimediaField],
   slug: "mediaBlock",
 };
---
+++ src/payload/custom-fields/multimedia.ts
@@ -0,0 +1,43 @@
+import { Field } from "payload";
+
+import { tl } from "@/i18n/translations";
+
+import { richTextField } from "@/payload/fields/rich-text";
+import { selectField } from "@/payload/fields/select";
+import { uploadField } from "@/payload/fields/upload";
+
+export const multimediaField: Field = {
+  localized: true,
+  label: false,
+  name: "multimedia",
+  type: "group",
+
+  fields: [
+    selectField({
+      defaultValue: "default",
+      label: tl("custom:media-position"),
+      name: "position",
+      options: [
+        {
+          label: tl("custom:media-position-default"),
+          value: "default",
+        },
+        {
+          label: tl("custom:media-position-wide"),
+          value: "wide",
+        },
+      ],
+    }),
+
+    richTextField({
+      label: tl("custom:media-caption"),
+      name: "caption",
+    }),
+
+    uploadField({
+      label: tl("custom:media-image"),
+      required: true,
+      name: "media",
+    }),
+  ],
+};
