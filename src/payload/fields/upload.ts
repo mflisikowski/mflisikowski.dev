bbcc3ccac7fb547cd0a20641b7f85469e5813754
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-11 20:57:25 +0100

    refactor: use translation helper for upload field label

--- src/payload/fields/upload.ts
+++ src/payload/fields/upload.ts
@@ -1,16 +1,16 @@
 import type { UploadField } from "payload";
 import { deepMerge } from "payload";
 
+import { tl } from "@/i18n/translations";
+
 export type UploadFieldProps = (overrides?: Partial<UploadField>) => UploadField;
 
 export const uploadField: UploadFieldProps = (overrides = {}) =>
   deepMerge<UploadField>(
     {
-      // @ts-expect-error - TFunction type is not automatically merged with the default translations
-      label: ({ t }) => t("custom-upload"),
-
       relationTo: "media",
       localized: true,
+      label: tl("custom-upload"),
       name: "upload",
       type: "upload",
     },
