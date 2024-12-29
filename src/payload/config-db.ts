fa662ac3a5844b84d39b397307d7fd1584d50ba0
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-12-29 08:16:59 +0100

    feat(config-db): add UUID support for database configuration
    - Introduced 'idType' set to 'uuid' in the database configuration to enhance identifier management and ensure consistency across database entries.

--- src/payload/config-db.ts
+++ src/payload/config-db.ts
@@ -2,6 +2,7 @@ import { postgresAdapter } from "@payloadcms/db-postgres";
 import type { Config } from "payload";
 
 export const db: Config["db"] = postgresAdapter({
+  idType: "uuid",
   pool: {
     connectionString: process.env.DATABASE_URI!,
   },
