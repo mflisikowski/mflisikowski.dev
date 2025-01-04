386f8aa6fe23d9eda9de679c0ea6bf0aec6446a6
Mateusz Flisikowski <contact@mflisikowski.dev>
2025-01-04 13:56:19 +0100

    fix(content-block): change required field to optional

--- src/payload/custom-fields/content-block.ts
+++ src/payload/custom-fields/content-block.ts
@@ -11,7 +11,7 @@ export const contentBlockField: PayloadBlockFieldProps = (overrides = {}) =>
   deepMerge<BlocksField>(
     {
       localized: true,
-      required: true,
+      required: false,
       blocks: [
         //
         ContentBlock,
