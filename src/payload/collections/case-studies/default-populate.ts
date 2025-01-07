aba0e8e26947a47676be251af18acfc002f2d1d1
Mateusz Flisikowski <contact@mflisikowski.dev>
2025-01-07 09:01:59 +0100

    refactor(case-studies): remove featuredImage from default populate configuration

--- src/payload/collections/case-studies/default-populate.ts
+++ src/payload/collections/case-studies/default-populate.ts
@@ -1,7 +1,6 @@
 import type { CollectionConfig } from "payload";
 
 export const caseStudiesDefaultPopulate: CollectionConfig["defaultPopulate"] = {
-  featuredImage: true,
   title: true,
   slug: true,
   url: true,
